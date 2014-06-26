48baa7cd24f01c917298d1325eb2514e76c52d9b
BrendanBenshoof <brendan@glidr.net>
2014-06-26 12:59:22 -0400

    subtraction works
    still need to check to slip sign when adding mixed sign numbers

--- BigInt.nim
+++ BigInt.nim
@@ -5,7 +5,7 @@ type
     digits:BigIntDigits  #posetive digits in base maxvalue(uint)+1
     neg:bool #true if negative, false if zero or posetive
 
-var maxInt: int64 = 2 shl 31
+var maxInt: int64 = int64(high(int32))
 
 echo(maxInt)
 
@@ -13,41 +13,22 @@ proc initBigInt *(val: uint32): BigInt =
   result.digits = cast[BigIntDigits](@[val])
   result.neg = false
 
-proc `-` *(A:BigInt,B:BigInt) : BigInt=
+
+proc invert(A:BigInt) : BigInt = 
   result = initBigInt(0)
+  result.neg = not result.neg
   var digits : BigIntDigits = @[]
-  var i: int = 0
-  var carry: int64 = 0
-  while i < A.digits.len or i < B.digits.len:
-    var tmp: int64
-    if i < A.digits.len:
-      if i < B.digits.len:
-        tmp = int64(A.digits[i]) - int64(B.digits[i]) - carry
-        if tmp >= maxInt: #we overflowed
-          carry = 1
-          tmp=tmp - maxInt
-        else:
-          carry = 0
-      else:
-        tmp = int64(A.digits[i]) - carry
-        if tmp >= maxInt: #we overflowed
-          carry = 1
-          tmp=tmp - maxInt
-        else:
-          carry = 0
-    else:
-      tmp = int(B.digits[i]) - carry
-      if tmp >= maxInt: #we overflowed
-        carry = 1
-        tmp=tmp - maxInt
-      else:
-        carry = 0
+  var i: int = A.digits.len -1
+  while i >= 0:
+    var tmp: int64 = int64(A.digits[i])
+    tmp = tmp * -1
     digits.add(cast[uint32](tmp))
-    i = i + 1
-  if carry > 0:
-    digits.add(cast[uint32](carry))
+    i = i - 1
   result.digits = digits
 
+
+proc `-` *(A:BigInt,B:BigInt) : BigInt=
+  result = A + invert(B)
 
 proc `+` *(A:BigInt,B:BigInt) : BigInt=
   result = initBigInt(0)
@@ -86,9 +67,9 @@ proc `+` *(A:BigInt,B:BigInt) : BigInt=
 
   
 
-var A :BigInt = initBigInt(12)
+var A :BigInt = initBigInt(2)
 var B :BigInt = initBigInt(10)
 
 
-let C: BigInt = B-A
+let C: BigInt = B+invert(A)
 echo(C.digits)
